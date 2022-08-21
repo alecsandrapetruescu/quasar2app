d17738bda79ec6907ecff1d8ee5a037a19a7ce3b
alecsandrapetruescu <alecsandra.petruescu@gmail.com>
2022-08-21 11:20:03 +0300

    Updated DefaultLayout

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,27 +1,28 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="lHh Lpr lFf" >
     <q-header elevated>
-      <q-toolbar class="text-white">
-        <q-toolbar-title>
-          <q-btn flat label="" icon="home" />
-        </q-toolbar-title>
-
-        <q-tabs
-          v-model="tab"
-          active-color="white"
-          shrink
-          mobile-arrows
-          inline-label
-        >
-          <q-route-tab name="home" label="Domain" to="/" />
-          <q-separator dark vertical inset />
-          <q-route-tab name="about" label="About" to="/about" />
-          <q-route-tab name="question" label="Question" to="/question" />
-          <q-route-tab name="contact" label="Contact" to="/contact" />
-          <q-separator dark vertical inset />
-          <q-route-tab name="admin" icon="settings" to="/admin" />
-        </q-tabs>
-      </q-toolbar>
+      <div class="row no-wrap">
+        <q-toolbar class="text-white offset-1 col-9">
+          <q-toolbar-title>
+            <q-tabs
+                v-model="tab"
+                active-color="white"
+                shrink
+                mobile-arrows
+                inline-label
+                align="right"
+            >
+              <q-route-tab name="home" label="Domain" to="/" />
+              <q-separator dark vertical inset />
+              <q-route-tab name="about" label="About" to="/about" />
+              <q-route-tab name="question" label="Question" to="/question" />
+              <q-route-tab name="contact" label="Contact" to="/contact" />
+              <q-separator dark vertical inset />
+              <q-route-tab name="admin" label="Sign in" to="/admin" />
+            </q-tabs>
+          </q-toolbar-title>
+        </q-toolbar>
+      </div>
     </q-header>
 
     <q-page-container>
