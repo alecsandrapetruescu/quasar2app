246e37d4be737efa2cbd5a45784dd50c829eef25
alecsandrapetruescu <alecsandra.petruescu@gmail.com>
2022-08-21 11:47:58 +0300

    Updated About page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,26 +1,29 @@
 <template>
   <q-page class="flex flex-center">
     <div class="fit row wrap justify-center items-start content-start">
-    <div class="col-10">
-      <q-card flat bordered >
-        <q-item>
-          <q-item-section avatar>
-            <q-skeleton type="QAvatar" animation="fade" />
-          </q-item-section>
+      <div class="col-10">
+        <q-card flat bordered>
+          <q-item>
+            <q-item-section avatar>
+              <q-skeleton bordered type="QAvatar" animation="fade" class="bg-blue-1"/>
+            </q-item-section>
 
-          <q-item-section>
-            <q-item-label>
-              <q-skeleton type="text" animation="fade" />
-            </q-item-label>
-            <q-item-label caption>
-              <q-skeleton type="text" animation="fade" />
-            </q-item-label>
-          </q-item-section>
-        </q-item>
+            <q-item-section>
+              <q-item-label>
+                <q-skeleton bordered type="text" animation="fade"/>
+              </q-item-label>
+              <q-item-label caption>
+                <q-skeleton bordered type="text" animation="fade"/>
+              </q-item-label>
+              <q-item-label caption>
+                <q-skeleton bordered type="text" animation="fade"/>
+              </q-item-label>
+            </q-item-section>
+          </q-item>
 
-        <q-skeleton height="200px" square animation="fade" />
-      </q-card>
-    </div>
+          <q-skeleton bordered height="600px" square animation="fade"/>
+        </q-card>
+      </div>
     </div>
   </q-page>
 </template>
